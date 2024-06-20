c268fae7373237fdf567537eb76ef730eac3a3be
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-20 20:57:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,94 @@
-// functions/whitelist.js
-const { MessageEmbed } = require('discord.js');
-const { MessageButton, MessageActionRow } = require('discord-buttons');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+const { printWatermark } = require('./functions/handlers');
 
-const APPLICATION_CHANNEL_ID = '1253323014003757189'; // Replace with your application channel ID
 
-module.exports = {
-  handleWhitelistApplication: async function (client, message) {
-    if (message.channel.id !== APPLICATION_CHANNEL_ID) return;
 
-    const embed = new MessageEmbed()
-      .setTitle('Apply for Whitelist')
-      .setDescription('Click the button below to apply for whitelist.')
-      .setColor('#0099ff');
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
 
-    const applyButton = new MessageButton()
-      .setLabel('Apply Now')
-      .setStyle('blurple')
-      .setID('apply_button');
+const prefix = '!';
+client.commands = new Map(); 
 
-    const row = new MessageActionRow().addComponents(applyButton);
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-    const sentMessage = await message.channel.send({ embeds: [embed], components: [row] });
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
-    client.on('clickButton', async (button) => {
-      if (button.id === 'apply_button' && button.message.id === sentMessage.id) {
-        const dmChannel = await button.clicker.user.createDM();
-        const responseEmbed = new MessageEmbed()
-          .setTitle('Application Submitted')
-          .setDescription('Your application has been submitted successfully.')
-          .setColor('#00ff00');
-        dmChannel.send({ embeds: [responseEmbed] });
 
-        // Optionally, you can handle further processing of the application here
-      }
+client.on('messageCreate', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName);
+
+  if (!command) return;
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command!');
+  }
+});
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+});
+printWatermark();
+
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log('\x1b[32m%s\x1b[0m', '|    🍔 Bot logged in successfully!');
+    console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!');
+    console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
+    console.log('\x1b[36m%s\x1b[0m', `|    🏡 Bot is in ${client.guilds.cache.size} servers`);
+  } catch (error) {
+    console.error('\x1b[31m%s\x1b[0m', '❌ Failed to log in:', error);
+    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Login, Restarting Process...');
+    process.kill(1);
+  }
+}
+
+
+client.once('ready', () => {
+  setTimeout(() => {
+    console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
+    client.user.setPresence({
+      activities: [{ name: `Watching You !`, type: ActivityType.Custom }],
+      status: 'dnd',
     });
-  },
-};
+  }, 2000); 
+});
+
+
+login();
+
+
+setInterval(() => {
+  if (!client || !client.user) {
+    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Logged in, Restarting Process...');
+    process.kill(1);
+  }
+}, 15000);
+
+module.exports = client;
